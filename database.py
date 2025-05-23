581122c9bab7d3421ec043090ea66cee03854ea8
Titouan Bonnet Plomb <titouan.plomb@outlook.com>
2025-05-23 16:48:29 +0200

    Use root to connect sql database

--- database.py
+++ database.py
@@ -16,11 +16,10 @@ APP_ENV = os.getenv("APP_ENV", "dev")
 
 if APP_ENV == "prod":
     DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
     DATABASE_HOST = os.getenv("DATABASE_HOST")
     DATABASE_PORT = os.getenv("DATABASE_PORT")
     DATABASE_NAME = os.getenv("DATABASE_NAME")
-    DATABASE_URL = f"mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    DATABASE_URL = f"mysql+pymysql://{DATABASE_USERNAME}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
     connect_args = {}
 else:
     DATABASE_URL = "sqlite:///./items.db"
