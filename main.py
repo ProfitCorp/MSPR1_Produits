37e3666300e0e9305da97361b9be1493ad0b329d
t2plb <titouan.plomb@outlook.com>
2025-05-25 16:33:07 +0200

    Add init.py to init admin user and .env.example

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ by creating all defined models, and includes the API routes from the `routes` mo
 - `Base.metadata.create_all(bind=engine)` ensures all tables defined in the ORM models are created.
 - `FastAPI()` creates the app instance.
 - `app.include_router(routes.router)` includes all route definitions from the routes module.
-- `APP_ENV` define which environnement is used to run application.
+- `init_admin_user` initialize admin user if doesn't exist
 """
 from fastapi import FastAPI
 from database import Base, engine
+from init import init_admin_user
 import routes
 import os
 
 Base.metadata.create_all(bind=engine)
+
+init_admin_user()
 
 app = FastAPI()
 
